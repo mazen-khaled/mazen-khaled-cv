6bf7d8a73716e99bb8cdedb945e7257703399246
mazen-khaled <64374092+mazen-khaled@users.noreply.github.com>
2021-07-16 20:04:16 +0200

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,11 @@
+function opens() {
+    document.getElementById("cl").style.transition = "all ease 0.3s";
+    document.getElementById("cl").style.display = "block";
+    document.getElementById("bm").style.display = "block";
+   
+}
+function closes() {
+    document.getElementById("cl").style.transition = "all ease 0.3s";
+    document.getElementById("cl").style.display = "none";
+    document.getElementById("bm").style.display = "none";
+}
